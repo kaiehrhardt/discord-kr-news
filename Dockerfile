c01543f79a827d301bd4aa332269f39c9f52f89a
Kai Ehrhardt <ehrhardt.kai@gmail.com>
2023-03-05 21:22:40 +0100

    feat: add discord bot content + dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:1.19-alpine as build
+WORKDIR /app
+COPY . .
+RUN go mod download && CGO_ENABLED=0 go build
+
+FROM gcr.io/distroless/static-debian11
+COPY --from=build /app/discord-kr-news /usr/local/bin/
+ENV KRN_DISCORD_TOKEN=dummy
+ENV KRN_DISCORD_CHANNEL=dummy
+ENV KRN_CHECK_INTERVAL=600s
+ENTRYPOINT ["discord-kr-news"]
--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/bwmarrin/discordgo"
 )
 
 const (
 	baseUrl = "https://www.nexon.com/kartdrift/de/news/"
+)
+
+var (
+	discordToken     = os.Getenv("KRN_DISCORD_TOKEN")
+	discordChannel   = os.Getenv("KRN_DISCORD_CHANNEL")
+	checkInterval, _ = time.ParseDuration(os.Getenv("KRN_CHECK_INTERVAL"))
 )
 
 type News struct {
@@ -18,25 +26,32 @@ type News struct {
 }
 
 type Post struct {
-	Url      string
-	Category string
+	Url string
 }
 
-func (n *News) AddPost(url string, category string) {
-	n.Posts = append(n.Posts, Post{Url: url, Category: category})
+func (n *News) AddPost(url string) {
+	n.Posts = append(n.Posts, Post{Url: url})
 }
 
-func (n *News) Print() error {
+func (n *News) Print() {
 	if len(n.Posts) <= 1 {
-		return errors.New("Empty Posts?!")
+		log.Fatalln("Empty Posts?!")
+	} else {
+		log.Println(n.Posts)
 	}
-	for _, p := range n.Posts {
-		fmt.Println(p.Url)
-	}
-	return nil
 }
 
-func Init() *News {
+func Compare(oldNews, newNews *News) (diffUrls []string) {
+	for i, post := range oldNews.Posts {
+		if post.Url != newNews.Posts[i].Url {
+			diffUrls = append(diffUrls, newNews.Posts[i].Url)
+			log.Println("Diff found. Old: %s New: %s", post.Url, newNews.Posts[i].Url)
+		}
+	}
+	return diffUrls
+}
+
+func GetNews() *News {
 	var categories = []string{"announcement", "update", "ingameevent", "communityevent"}
 	n := &News{}
 	for _, category := range categories {
@@ -52,16 +67,42 @@ func Init() *News {
 
 		doc.Find(".board_list").Each(func(i int, s *goquery.Selection) {
 			url, _ := s.Find("a").Attr("href")
-			n.AddPost(baseUrl+category+"/"+url, category)
+			n.AddPost(baseUrl + category + "/" + url)
 		})
 	}
 	return n
 }
 
 func main() {
-	n := Init()
-	err := n.Print()
+	dg, err := discordgo.New("Bot " + discordToken)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
+	}
+
+	_, err = dg.ChannelMessageSend(discordChannel, fmt.Sprintf("KR-News für Channel aktiviert."))
+	if err != nil {
+		log.Println(err)
+	}
+
+	initNews := GetNews()
+	initNews.Print()
+
+	for {
+		newNews := GetNews()
+		newNews.Print()
+		diff := Compare(initNews, newNews)
+
+		if len(diff) > 0 {
+			for _, news := range diff {
+				message := fmt.Sprintf("Es gibt eine neue News: %s", news)
+				_, err = dg.ChannelMessageSend(discordChannel, message)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+
+		time.Sleep(checkInterval)
 	}
 }
